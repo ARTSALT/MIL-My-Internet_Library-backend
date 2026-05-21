508ffedf3d7a6d4335d17d164ae5410ebba5a62c
ARTSALT <artur.guedes@alunos.ufersa.edu.br>
2026-05-21 16:03:29 -0300

    first commit

---
+++ src/main/java/com/project/mil/MilApplication.java
@@ -0,0 +1,13 @@
+package com.project.mil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilApplication.class, args);
+	}
+
+}
