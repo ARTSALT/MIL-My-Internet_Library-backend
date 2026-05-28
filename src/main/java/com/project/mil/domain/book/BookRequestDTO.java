640a746dfd2bbd42f57e754a5239e026753dda6a
ARTSALT <artur.guedes@alunos.ufersa.edu.br>
2026-05-28 14:54:34 -0300

    added compatibility between json snake case and java's camel case to the book request dto

--- src/main/java/com/project/mil/domain/book/BookRequestDTO.java
+++ src/main/java/com/project/mil/domain/book/BookRequestDTO.java
@@ -1,5 +1,15 @@
 package com.project.mil.domain.book;
 
-public record BookRequestDTO(String title, String authorName, Integer publishYear, String synopsis, String isbn) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record BookRequestDTO(
+    String title,
+    @JsonProperty("author_name")
+    String authorName,
+    @JsonProperty("publish_year")
+    Integer publishYear,
+    String synopsis,
+    String isbn
+) {
     
 }
